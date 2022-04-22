6fe91be6f202f2f3492e974f0bb884c0fd13405d
mpbelz <mpbelz@gmail.com>
2022-04-22 14:00:32 -0500

    Class 3 Coding Challenge

---
+++ Class3CodingChallenge/index.js
@@ -0,0 +1,21 @@
+function FirstReverse(str) { 
+  // code goes here  
+  return str.split('').reverse().join(''); 
+
+}
+   
+// keep this function call here 
+console.log(FirstReverse("Hello"));
+
+
+
+//This was the provided starter code so
+
+// function FirstReverse(str) { 
+//   // code goes here  
+//   return str.split('').reverse().join('');; 
+
+// }
+   
+// // keep this function call here 
+// console.log(FirstReverse(readline()));
